2e4dadb94c0a9518f37b9b570331d17f7b075cc0
Simpleshaikh1 <simpleshaikh2@gmail.com>
2023-04-17 09:03:40 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementsByClassName('right-hamburger');
+const navMenu = document.getElementsByClassName('nav-li');
+
+function openNav() {
+    hamburger.style.display = "none";
+    navMenu.style.display = "block";
+}
+
+console.log(hamburger)
