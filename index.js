d80a62bc7616881e9d87afd0a7304ccf6f0c1dfd
husush23 <husush21@gmail.com>
2023-04-19 13:16:53 +0300

    menu function added

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const hamburgMenu = document.querySelector('.right-hamburger')
-const menuList = document.querySelector('.nav-li')
-const lists = document.querySelector('.lis')
-const xMen = document.querySelector('.x-men')
-const otherLink = document.querySelector('.lis')
+const hamburgMenu = document.querySelector('.right-hamburger');
+const menuList = document.querySelector('.nav-li');
+const lists = document.querySelector('.lis');
+const xMen = document.querySelector('.x-men');
+const otherLink = document.querySelector('.lis');
 
-hamburgMenu.addEventListener('click', function() {
-    menuList.classList.toggle('nav-active')
-})
+hamburgMenu.addEventListener('click', function () {
+  menuList.classList.toggle('nav-active');
+});
 
-xMen.addEventListener('click', function() {
-    menuList.classList.toggle('nav-active')
-})
+xMen.addEventListener('click', function () {
+  menuList.classList.toggle('nav-active');
+});
 
-
-
-
-
-
-
+menuList.addEventListener('click', function () {
+  menuList.classList.toggle('nav-active');
+});
