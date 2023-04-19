c84b0b0df534bffb0364135e2e6f82ce6f7880b5
Simpleshaikh1 <simpleshaikh2@gmail.com>
2023-04-19 11:36:15 +0100

    changed from crlf to lf

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const hamburgMenu = document.querySelector('.right-hamburger');
 const menuList = document.querySelector('.nav-li');
-const lists = document.querySelector('.lis');
 const xMen = document.querySelector('.x-men');
-const otherLink = document.querySelector('.lis');
 
-hamburgMenu.addEventListener('click', function () {
+hamburgMenu.addEventListener('click', () => {
   menuList.classList.toggle('nav-active');
 });
 
-xMen.addEventListener('click', function () {
+xMen.addEventListener('click', () => {
   menuList.classList.toggle('nav-active');
 });
 
-menuList.addEventListener('click', function () {
+menuList.addEventListener('click', () => {
   menuList.classList.toggle('nav-active');
 });
